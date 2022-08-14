540c33ba11b59e323cccab5ce66ee63929a7c033
zaty <zatyaty5258@gmail.com>
2022-08-14 17:44:37 +0800

    topbar

--- frontend/src/app/icons.js
+++ frontend/src/app/icons.js
@@ -1,10 +1,12 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 
-import { faMoon, faSun, faNewspaper, faLeaf, faBullseye } from "@fortawesome/free-solid-svg-icons";
+import { faMoon, faSun, faNewspaper, faLeaf, faBullseye, faCircleUser, faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 
 library.add(fab, faMoon);
 library.add(fab, faSun);
 library.add(fab, faNewspaper);
 library.add(fab, faLeaf);
 library.add(fab, faBullseye);
+library.add(fab, faCircleUser);
+library.add(fab, faRightFromBracket);
