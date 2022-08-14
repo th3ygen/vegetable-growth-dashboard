ff069ba4c1a5fb81f5183294b46a3ad3e6b65058
Aidil <aidil@lab.ump>
2022-08-15 00:53:23 +0800

    setting form

--- frontend/src/pages/user/Settings.jsx
+++ frontend/src/pages/user/Settings.jsx
@@ -1,6 +1,26 @@
-import { Form, Input, Button, message, Checkbox } from "antd";
+import {
+	Form,
+	Input,
+	Button,
+	message,
+	Checkbox,
+	Typography,
+	Divider,
+	Dropdown,
+	Menu,
+	Space,
+	Select,
+} from "antd";
+import {
+	DownOutlined,
+	PlusOutlined,
+	MinusCircleOutlined,
+} from "@ant-design/icons";
+import { useState } from "react";
 
 import styled from "styled-components";
+
+const { Title } = Typography;
 
 const Wrapper = styled.div`
 	box-shadow: 2.8px 2.8px 2.2px rgba(0, 0, 0, 0.02),
@@ -17,69 +37,140 @@ const Wrapper = styled.div`
 	padding: 20px;
 `;
 
+const ColorItem = styled.div`
+	display: flex;
+	align-items: center;
+	gap: 10px;
+`;
+
+const ColorItemBox = styled.div`
+	width: 10px;
+	height: 10px;
+
+	border-radius: 4px;
+
+	background-color: ${(props) => props.color};
+`;
+
 export default function Settings() {
-    const onFinish = values => {
-        console.log("Success:", values);
-    };
+	const colors = [
+		{
+			value: "#00ff00",
+			label: (
+				<ColorItem>
+					<ColorItemBox color="#00ff00" />
+					<span>Green</span>
+				</ColorItem>
+			),
+		},
+		{
+			value: "#ff0000",
+			label: (
+				<ColorItem>
+					<ColorItemBox color="#ff0000" />
+					<span>Red</span>
+				</ColorItem>
+			),
+		},
+	];
 
-    const onFinishFailed = errorInfo => {
-        console.log("Failed:", errorInfo);
+	const onFinish = (values) => {
+		console.log("Success:", values);
+	};
 
-        message.error("Failed to update settings");
-    };
+	const onFinishFailed = (errorInfo) => {
+		console.log("Failed:", errorInfo);
+
+		message.error("Failed to update settings");
+	};
 
 	return (
 		<>
 			<Wrapper>
+				<Title level={3}>Crops setting</Title>
+				<Divider />
 				<Form
 					name="basic"
-					initialValues={{
-						remember: true,
-					}}
-                    layout="vertical"
+					layout="vertical"
 					onFinish={onFinish}
 					onFinishFailed={onFinishFailed}
 					autoComplete="off"
 				>
-					<Form.Item
-						label="Username"
-						name="username"
-						rules={[
-							{
-								required: true,
-								message: "Please input your username!",
-							},
-						]}
-					>
-						<Input />
-					</Form.Item>
-
-					<Form.Item
-						label="Password"
-						name="password"
-						rules={[
-							{
-								required: true,
-								message: "Please input your password!",
-							},
-						]}
-					>
-						<Input.Password />
-					</Form.Item>
-
-					<Form.Item
-						name="remember"
-						valuePropName="checked"
-					>
-						<Checkbox>Remember me</Checkbox>
-					</Form.Item>
-
-					<Form.Item
-					>
-						<Button type="primary" htmlType="submit">
-							Submit
-						</Button>
-					</Form.Item>
+					<Form.List name="crops">
+						{(fields, { add, remove }) => {
+							return (
+								<>
+									<Form.Item>
+										<Button
+											type="dashed"
+											onClick={() => add()}
+											block
+											icon={<PlusOutlined />}
+										>
+											Add crop
+										</Button>
+									</Form.Item>
+									{fields.map((field) => (
+										<Space
+											key={field.key}
+											style={{
+												display: "flex",
+											}}
+										>
+											<Form.Item
+												label="Crop name"
+												name={[field.name, "name"]}
+												rules={[
+													{
+														required: true,
+														message:
+															"Please input crop name",
+													},
+												]}
+											>
+												<Input
+													style={{
+														width: "100%",
+													}}
+												/>
+											</Form.Item>
+											<Form.Item
+												label="Short form (key)"
+												name={[field.key, "key"]}
+												rules={[
+													{
+														required: true,
+														message:
+															"Please input your crop short form!",
+													},
+												]}
+											>
+												<Input />
+											</Form.Item>
+											<Form.Item
+												label="Crop"
+												name={[field.key, "crop"]}
+												rules={[
+													{
+														required: true,
+														message:
+															"Please select a crop or create a new one!",
+													},
+												]}
+											>
+												<Select options={colors} />
+											</Form.Item>
+											<MinusCircleOutlined
+												onClick={() =>
+													remove(field.name)
+												}
+											/>
+										</Space>
+									))}
+								</>
+							);
+						}}
+					</Form.List>
 				</Form>
 			</Wrapper>
 		</>
