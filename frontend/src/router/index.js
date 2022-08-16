13f968566efaa2ad13c81ab9ddc0786c3dd9119c
Aidil <aidil@lab.ump>
2022-08-16 15:50:06 +0800

    env

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,23 +1,26 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from "react-router-dom";
 
-import UserLayout from '../layouts/UserLayout';
-import PlantReport from '../pages/user/PlantReport';
-import GrowthObservation from '../pages/user/GrowthObservation';
-import CropEstimation from '../pages/user/CropsEstimation';
-import Setting from '../pages/user/Settings';
+import UserLayout from "../layouts/UserLayout";
+import PlantReport from "../pages/user/PlantReport";
+import GrowthObservation from "../pages/user/GrowthObservation";
+import CropEstimation from "../pages/user/CropsEstimation";
+import Setting from "../pages/user/Settings";
 
 export default function Router() {
-    return (
-        <Routes>
-            <Route path="user" element={<UserLayout />}>
-                <Route path="dashboard" element={<PlantReport />} />
-                <Route path="growth" element={<GrowthObservation />} />
-                <Route path="estimation" element={<CropEstimation />} />
-                <Route path="settings" element={<Setting />} />
+	return (
+		<Routes>
+			<Route path="user" element={<UserLayout />}>
+				<Route path="dashboard" element={<PlantReport />} />
+				<Route path="growth" element={<GrowthObservation />} />
+				<Route path="estimation" element={<CropEstimation />} />
+				<Route path="settings" element={<Setting />} />
 
-                {/* redirect from * */}
-            </Route>
-                <Route path="*" element={ <Navigate to="/user/dashboard" replace={true} />} />
-        </Routes>
-    )
+				{/* redirect from * */}
+			</Route>
+			<Route
+				path="/"
+				element={<Navigate to="/user/dashboard" replace={true} />}
+			/>
+		</Routes>
+	);
 }
